21b6f969e0a7c5e7c815fafc5d0243980ab4f187
Matt Ward <matt.ward@microsoft.com>
2022-01-17 18:08:19 +0000

    Add test msbuild task

---
+++ Test.Xam.BuildTaskUpdate/GetTasksAbi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Test.Xam.BuildTaskUpdate
+{
+	public class GetTasksAbi : Task
+	{
+		[Output]
+		public string AbiVersion { get; } = "1";
+
+		public override bool Execute ()
+		{
+			return true;
+		}
+	}
+}
+
